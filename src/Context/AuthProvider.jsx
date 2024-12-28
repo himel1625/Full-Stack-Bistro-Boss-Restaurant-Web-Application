b0c7d3511d6aad68ee11e1c921abc376af17acd0
himel1625 <himelmia1625@gmail.com>
2024-12-28 23:29:03 +0600

    implement context API

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,14 @@
+import { createContext } from 'react';
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const AuthInfo = {
+    PH: 'PH BOSS',
+  };
+  return (
+    <div>
+      <AuthContext.Provider value={AuthInfo}>{children}</AuthContext.Provider>
+    </div>
+  );
+};
+
+export default AuthProvider;
